13543a28589aaff272a8c47dfafd4e35490c02af
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-07-02 08:00:24 -0500

    Add project files.

---
+++ Infrastructure/E-Trade.Persistance/Context/ETradeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using E_TradeStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Trade.Persistence.Context
+{
+    public class ETradeDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=YAHYAERDOGAN; initial catalog=ETradeStoreDb; integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
